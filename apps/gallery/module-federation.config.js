b18eaff3f2b668ee3180589afa11ac22b245e182
Aleksandr Shatoba <shatoba97@gmail.com>
2022-04-28 21:57:59 +0300

    Improve configs

--- apps/gallery/module-federation.config.js
+++ apps/gallery/module-federation.config.js
@@ -1,6 +1,20 @@
+
+const mf = require('@angular-architects/module-federation/webpack');
+const path = require('path');
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, '../../tsconfig.base.json'), [
+  '@mf-app/shared/data-store',
+]);
+
 module.exports = {
   name: 'gallery',
   exposes: {
     './Module': 'apps/gallery/src/app/remote-entry/entry.module.ts',
   },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  }
 };
